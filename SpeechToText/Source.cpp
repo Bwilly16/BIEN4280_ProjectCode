2f29c88517ee93915b496872127bd9263d027790
Bwilly16 <benjamin.wilichowski@marquette.edu>
2022-11-22 13:36:40 -0600

    Trying to get serial comms

--- SpeechToText/Source.cpp
+++ SpeechToText/Source.cpp
@@ -1,12 +1,109 @@
 #include <iostream> 
 #include <stdlib.h>
+#include <stdio.h>
 #include <speechapi_cxx.h>
+#include <windows.h>
+#include <tchar.h>
 
 
 using namespace Microsoft::CognitiveServices::Speech;
 using namespace Microsoft::CognitiveServices::Speech::Audio;
 
 std::string GetEnvironmentVariable(const char* name);
+HANDLE hComm;
+//typedef const wchar_t* gszPort = ("COM9");
+TCHAR* pcCommPort = TEXT("COM9");
+
+
+
+void PrintCommState(DCB dcb)
+{
+    //  Print some of the DCB structure values
+    _tprintf(TEXT("\nBaudRate = %d, ByteSize = %d, Parity = %d, StopBits = %d\n"),
+        dcb.BaudRate,
+        dcb.ByteSize,
+        dcb.Parity,
+        dcb.StopBits);
+}
+
+
+int _tmain(int argc, TCHAR* argv[])
+{
+    DCB dcb;
+    HANDLE hCom;
+    BOOL fSuccess;
+    TCHAR* pcCommPort = TEXT("COM1"); //  Most systems have a COM1 port
+
+    //  Open a handle to the specified com port.
+    hCom = CreateFile(pcCommPort,
+        GENERIC_READ | GENERIC_WRITE,
+        0,      //  must be opened with exclusive-access
+        NULL,   //  default security attributes
+        OPEN_EXISTING, //  must use OPEN_EXISTING
+        0,      //  not overlapped I/O
+        NULL); //  hTemplate must be NULL for comm devices
+
+    if (hCom == INVALID_HANDLE_VALUE)
+    {
+        //  Handle the error.
+        printf("CreateFile failed with error %d.\n", GetLastError());
+        return (1);
+    }
+
+    //  Initialize the DCB structure.
+    SecureZeroMemory(&dcb, sizeof(DCB));
+    dcb.DCBlength = sizeof(DCB);
+
+    //  Build on the current configuration by first retrieving all current
+    //  settings.
+    fSuccess = GetCommState(hCom, &dcb);
+
+    if (!fSuccess)
+    {
+        //  Handle the error.
+        printf("GetCommState failed with error %d.\n", GetLastError());
+        return (2);
+    }
+
+    PrintCommState(dcb);       //  Output to console
+
+    //  Fill in some DCB values and set the com state: 
+    //  57,600 bps, 8 data bits, no parity, and 1 stop bit.
+    dcb.BaudRate = CBR_57600;     //  baud rate
+    dcb.ByteSize = 8;             //  data size, xmit and rcv
+    dcb.Parity = NOPARITY;      //  parity bit
+    dcb.StopBits = ONESTOPBIT;    //  stop bit
+
+    fSuccess = SetCommState(hCom, &dcb);
+
+    if (!fSuccess)
+    {
+        //  Handle the error.
+        printf("SetCommState failed with error %d.\n", GetLastError());
+        return (3);
+    }
+
+    //  Get the comm config again.
+    fSuccess = GetCommState(hCom, &dcb);
+
+    if (!fSuccess)
+    {
+        //  Handle the error.
+        printf("GetCommState failed with error %d.\n", GetLastError());
+        return (2);
+    }
+
+    PrintCommState(dcb);       //  Output to console
+
+    _tprintf(TEXT("Serial port %s successfully reconfigured.\n"), pcCommPort);
+    return (0);
+}
+
+void SerialComs() {
+    hComm = CreateFile(pcCommPort, GENERIC_READ | GENERIC_WRITE, 0, 0, OPEN_EXISTING, FILE_FLAG_OVERLAPPED, 0);
+    if (hComm == INVALID_HANDLE_VALUE)
+        // error opening port; abort
+}
 
 int main()
 {
@@ -61,6 +158,7 @@ int main()
             std::cout << "CANCELED: Did you set the speech resource key and region values?" << std::endl;
         }
     }
+    
 }
 
 
