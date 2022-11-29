e36516706ecd9714b8a7776dcecc84a0b810e092
erincahi19 <erin.cahill@marquette.edu>
2022-11-29 13:27:13 -0600

    Proximity code added
    Needs testing, but probably needs speech to text working to avoid commenting out/changing things

--- Project/TempSensorMain.cpp
+++ Project/TempSensorMain.cpp
@@ -4,15 +4,20 @@
 
 #define LEDDIR (uint32_t*) 0x50000514 
 #define SETTEMPERATURE (1UL << 8)
+#define SETPROXIMITY (1UL << 8)
 
 unsigned int readReg = 0xEF;
 unsigned int writeReg = 0xEE; 
 
+unsigned int readRegProx = (0x39<<1) + 1; //Read and write registers for proximity sensor
+unsigned int writeRegProx = 0x39<<1;
+
 Ticker interruptTicker;
 USBSerial test;
-Thread thread;
+Thread thread, thread1, thread2;
 EventFlags PTEvent;
 I2C i2c(p31, p2); //(SDA, SCL)
+DigitalOut port22(p22);
 DigitalOut pullupResistor(p32); //Pin 32 = P1_0
 DigitalOut redLED(LED2);
 DigitalOut blueLED(LED4);
@@ -163,21 +168,86 @@ void read_temperature(){
     }
 }
 
+void proximity_sensor(){
+    char Data[8] = {0, 0, 0, 0, 0, 0, 0, 0}; //char == uint8
+    int i = 0;
+    long setProx = 0;
+
+    //setProx = result; //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    test.printf("readReg: %i\r\n", readRegProx);
+    test.printf("writeReg: %i\r\n\r\n", writeRegProx);
+
+    thread_sleep_for(10);
+
+    Data[0] = 0x92; //146
+    i2c.write(writeRegProx, Data, 1, true); //Check ID
+    i2c.read(readRegProx, Data, 1); //Expect a 0xAB = 171 back
+
+    //test.printf("The sensor ID is: %d\r\n\r\n", Data[0]);
+
+    if(Data[0] == 171){
+            Data[0] = 0x90; //Increasing distance
+            Data[1] = 0x30;
+            i2c.write(writeRegProx, (const char *)Data, 2);
+
+            Data[0] = 0x8F; //Increasing gain = improves readings of distant signals
+            Data[1] = 0x0C;
+            i2c.write(writeRegProx, (const char *)Data, 2);
+
+        while(true){
+            thread_sleep_for(50); //5 millisecond buffer
+
+            Data[0] = 0x80; //Send start signal
+            Data[1] = 0x25;
+            i2c.write(writeRegProx, (const char *)Data, 2);
+
+            thread_sleep_for(15);
+
+            Data[0] = 0x9C; //Read data obtained
+            i2c.write(writeRegProx, (const char *)Data, 1, true);
+            i2c.read(readRegProx, Data, 1);
+
+            //test.printf("PDATA is: %d\r\n", Data[i]); //Data from chip
+
+            if((Data[0] < (setProx + 1)) && (Data[0] > (setProx - 1))) { //Estimated calibrations
+                test.printf("%i cm reached\r\n", setProx);
+            }
+        }
+    }
+}
+
 void setEFlag(){ //Send event flag to read_temperature
-    PTEvent.set(SETTEMPERATURE);
+    //if(result == 'Activate temperature sensor'){
+        PTEvent.set(SETTEMPERATURE);
+    //}
+    //else if(result == 'Activate proximity sensor'){
+        PTEvent.set(SETPROXIMITY);
+    //}
+    //else if(result == 'Activate color sensor'){
+    // PTEvent.set(SETCOLOR);
+    //}
+    //else(){
+        test.printf("Please activate a valid sensor\r\n\n");
+        test.printf("VALID COMMANDS:\r\n");
+        test.printf("'Activate temperature sensor'\r\n'Activate proximity sensor'\r\n'Activate color sensor'");
+    //}
 }
 
 int main() {
     thread.start(read_temperature);
-	*LEDDIR = *LEDDIR | (1 << 6);
-    *LEDDIR = *LEDDIR | (1 << 24);
+    thread1.start(proximity_sensor);
+	//*LEDDIR = *LEDDIR | (1 << 6);
+    //*LEDDIR = *LEDDIR | (1 << 16);
+    //*LEDDIR = *LEDDIR | (1 << 24);
 
     redLED = 1; //Turns LED off when first setting bits, causes a short blink
     blueLED = 1;
     greenLED = 1;
     pullupResistor = 1;
+    port22 = 1;
 
-    interruptTicker.attach(&setEFlag, 3.0);
+    interruptTicker.attach(&setEFlag, 1.0);
 
     while (true) {
         thread_sleep_for(1000);
